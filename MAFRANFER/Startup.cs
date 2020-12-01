a69980793e6999edcc114cbc77b8a1ae5888c8a2
Velfra <velfra15.ov@gmail.com>
2020-12-01 09:40:09 -0300

    Agregar archivos de proyecto.

---
+++ MAFRANFER/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MAFRANFER.Startup))]
+namespace MAFRANFER
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
